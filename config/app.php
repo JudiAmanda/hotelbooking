7dafc5ff7e91b310ec46945c724b779e620704e5
judith <judichris35@gmail.com>
2023-11-24 10:41:53 +0000

    error log setting

--- config/app.php
+++ config/app.php
@@ -31,6 +31,8 @@ return [
 
     'env' => env('APP_ENV', 'production'),
 
+    'log' => 'errorlog'
+
     /*
     |--------------------------------------------------------------------------
     | Application Debug Mode
